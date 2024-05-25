e010e2cf408fa642e648d93e6777f75c1a65aa36
Yugveer Singh Wadzatia <yugveersingh06@gmail.com>
2024-05-25 11:51:37 +0530

    added loader text to redirection page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -10,10 +10,13 @@ import {
 } from "firebase/firestore";
 import { Loader2 } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { motion as m } from "framer-motion";
+
 function Redirect() {
     const router = useRouter();
     const { id } = useParams();
+    const [loaderText, setLoaderText] = useState<String | null>(null);
 
     useEffect(() => {
         getDocs(query(collection(db, "links"), where("shortId", "==", id)))
@@ -25,7 +28,7 @@ function Redirect() {
                     author: doc.data().author,
                     visits: doc.data().visits,
                 }));
-
+                setLoaderText("Redirecting to " + data[0].url);
                 updateDoc(doc(db, "links", data[0].id), {
                     visits: data[0].visits + 1,
                 })
@@ -39,8 +42,24 @@ function Redirect() {
             });
     }, []);
     return (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
             <Loader2 size={48} className="animate-spin text-teal-600" />
+            {loaderText && (
+                <m.span
+                    initial={{ opacity: 0, height: 0, marginTop: 0, y: 64 }}
+                    animate={{
+                        opacity: 1,
+                        height: "auto",
+                        marginTop: 16,
+                        y: 0,
+                        transition: {
+                            ease: [0, 0.75, 0.25, 1],
+                        },
+                    }}
+                >
+                    {loaderText}
+                </m.span>
+            )}
         </div>
     );
 }
